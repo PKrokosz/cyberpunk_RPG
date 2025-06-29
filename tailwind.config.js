d954e78597c8756bbe4068bce8a9e1f4a4f0efbd
PKrokosz <98399042+PKrokosz@users.noreply.github.com>
2025-06-29 09:53:08 +0200

    [GLITCHWAVE] add neon utility classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: ["./index.html", "./js/**/*.js", "./src/**/*.{ts,tsx,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'neon-magenta': '#ff00ff',
+        'neon-cyan': '#00ffff',
+      },
+      boxShadow: {
+        glow: '0 0 8px var(--neon-cyan)',
+      },
+    },
   },
   plugins: [],
 };
