22bdd76bde33a9b930d91d8a065fb496cdf5f4d4
PKrokosz <98399042+PKrokosz@users.noreply.github.com>
2025-06-29 11:16:58 +0200

    [GLITCHWAVE] Add HUD layout with sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import { NavBar } from './components/NavBar';
+import HUDLayout from './components/HUDLayout';
 import TerminalPage from './pages/TerminalPage';
 import JournalPage from './pages/JournalPage';
 import CharacterPage from './pages/CharacterPage';
 import InventoryPage from './pages/InventoryPage';
 import MapPage from './pages/MapPage';
+import SystemPage from './pages/SystemPage';
 import { Routes, Route } from 'react-router-dom';
 
 const App: React.FC = () => {
   return (
     <div className="min-h-screen bg-black text-green-400 font-mono">
-      <NavBar />
-      <main className="pt-20 px-8 space-y-12 mb-12">
-        <Routes>
-          <Route path="/" element={<TerminalPage />} />
-          <Route path="/terminal" element={<TerminalPage />} />
-          <Route path="/journal" element={<JournalPage />} />
-          <Route path="/character" element={<CharacterPage />} />
-          <Route path="/inventory" element={<InventoryPage />} />
-          <Route path="/map" element={<MapPage />} />
-        </Routes>
-      </main>
+      <Routes>
+        <Route element={<HUDLayout />}> 
+          <Route index element={<TerminalPage />} />
+          <Route path="terminal" element={<TerminalPage />} />
+          <Route path="map" element={<MapPage />} />
+          <Route path="journal" element={<JournalPage />} />
+          <Route path="character" element={<CharacterPage />} />
+          <Route path="system" element={<SystemPage />} />
+          <Route path="inventory" element={<InventoryPage />} />
+        </Route>
+      </Routes>
     </div>
   );
 };
